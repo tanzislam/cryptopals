cfa842c0bf9ddf592d75dfec7706a6680bae540a
Tanzinul Islam <t_17_7@hotmail.com>
2016-08-06 16:24:56 +0100

    BUILD: Dependency generation for compilations + better unification

--- common_settings.mk
+++ common_settings.mk
@@ -1,17 +1,45 @@
-ifeq "" "$(BOOST_DIR)"
-$(error The BOOST_DIR variable must be set to where you installed Boost v1.61.0)
-else ifeq "" "$(GTEST_DIR)"
-$(error The GTEST_DIR variable must be set to where you downloaded or cloned\
-        Google Test and Google Mock)
+# Validate that we have everything we need
+ifeq "" "$(SRCS)"
+    $(error SRCS must be defined before including this file)
+else ifneq "clean" "$(filter clean,$(MAKECMDGOALS))"
+    ifeq "" "$(BOOST_DIR)"
+        $(error The BOOST_DIR variable must be set to where you installed Boost\
+                v1.61.0)
+    else ifeq "" "$(GTEST_DIR)"
+        $(error The GTEST_DIR variable must be set to where you downloaded or\
+                cloned Google Test and Google Mock)
+    endif
 endif
 
+# Standard settings
 CXX = g++
 CPPFLAGS = -std=c++11 -Wc++11-compat -Wc++14-compat -pedantic -Wall -Wextra -O9
 CXXFLAGS = -isystem $(BOOST_DIR) -isystem $(GTEST_DIR)/googletest/include
 LDLIBS = $(GTEST_DIR)/googletest/make/gtest_main.a -lpthread
 
+# This is so that a Challenge-specific makefile can "include" the directory of
+# another Challenge using VPATH for both dependencies and C++ headers.
+#
+# Precondition: VPATH must be space-delimited, and not colon- or
+#               semicolon-delimited.
+CXXFLAGS += $(foreach v,$(VPATH),-I$(v))
+
+# The built-in rule for linking object files uses the $(CC) variable, which
+# expands to "cc". However, the "cc" program does not exist in a standard
+# MinGW-w64 installation, so using $(CXX) instead as defined above.
+CC = $(CXX)
+
+# Generate compilation dependencies from SRCS. We will still rely on implicit
+# rules for the compilations.
+ifneq "clean" "$(filter clean,$(MAKECMDGOALS))"
+    $(foreach \
+        s, \
+        $(wildcard $(SRCS)), \
+        $(eval $(shell $(CXX) -MMD -E $(CPPFLAGS) $(CXXFLAGS) $(s))) \
+    )
+endif
+
+# Standard cleanup target
 .PHONY : clean
 clean :
 	rm -f *.o *.exe
-
-.DEFAULT_GOAL =
