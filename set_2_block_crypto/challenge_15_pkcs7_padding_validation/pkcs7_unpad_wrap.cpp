f57cb80c527cdcaa36546a40f431cbfd6db2145a
Tanzinul Islam <t_17_7@hotmail.com>
2018-06-03 11:59:29 +0100

    Fix istream exceptions issue with Clang/libc++

--- set_2_block_crypto/challenge_15_pkcs7_padding_validation/pkcs7_unpad_wrap.cpp
+++ set_2_block_crypto/challenge_15_pkcs7_padding_validation/pkcs7_unpad_wrap.cpp
@@ -16,7 +16,9 @@ std::string unpad_pkcs7(const std::string & input)
 
     pkcs7_unpad_streambuf unpadder(instr, input.size());
     std::istream unpadStream(&unpadder);
-    unpadStream.exceptions(std::istream::failbit);
+    // Exception on failbit should be enought, but Clang/libc++ demands badbit.
+    // https://stackoverflow.com/questions/35019687
+    unpadStream.exceptions(std::istream::failbit | std::istream::badbit);
 
     std::string output;
     std::getline(unpadStream, output, '\0');
