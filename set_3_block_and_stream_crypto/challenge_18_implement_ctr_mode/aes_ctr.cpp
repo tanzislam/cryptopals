d2f6fab5e0fb11af8418c35bbb9a8a5c0ab64097
Restyled.io <commits@restyled.io>
2019-10-22 13:28:13 +0000

    Restyled by clang-format

--- set_3_block_and_stream_crypto/challenge_18_implement_ctr_mode/aes_ctr.cpp
+++ set_3_block_and_stream_crypto/challenge_18_implement_ctr_mode/aes_ctr.cpp
@@ -16,7 +16,7 @@ aes_ctr_streambuf::aes_ctr_streambuf(std::istream & input,
           d_nonce(nonce),
           d_counter()
 {
-    setupNextBlock(); 
+    setupNextBlock();
 }
 
 
@@ -25,9 +25,11 @@ void aes_ctr_streambuf::setupNextBlock()
     std::string nonceAndCounter;
     nonceAndCounter.reserve(CryptoPP::AES::BLOCKSIZE);
     nonceAndCounter
-        .append(reinterpret_cast<char*>(boost::endian::little_uint64_buf_t(d_nonce).data()), 
+        .append(reinterpret_cast<char *>(
+                    boost::endian::little_uint64_buf_t(d_nonce).data()),
                 sizeof(boost::endian::little_uint64_buf_t))
-        .append(reinterpret_cast<char*>(boost::endian::little_uint64_buf_t(d_counter++).data()),
+        .append(reinterpret_cast<char *>(
+                    boost::endian::little_uint64_buf_t(d_counter++).data()),
                 sizeof(boost::endian::little_uint64_buf_t));
 
     d_encryptedNonceAndCounter.resize(nonceAndCounter.size());
