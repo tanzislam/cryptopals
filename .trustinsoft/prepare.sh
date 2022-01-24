5614d926a601034aa9d63fcb6795e47d70970b79
Tanzinul Islam <11808226+tanzislam@users.noreply.github.com>
2022-01-24 09:16:29 +0000

    Remove Git commands + Install lsb_release in TrustInSoft CI (AWS)
    https://forums.aws.amazon.com/thread.jspa?messageID=519816#519816

--- .trustinsoft/prepare.sh
+++ .trustinsoft/prepare.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-env
-cat ~/.gitconfig
-cat .git/config
-git --version
-echo $BASH_VERSION
+sudo yum install redhat-lsb
 cd ..
-git pull --prune --recurse-submodules --jobs 8
-git submodule update --init --recursive --jobs 8
 SKIP_COMPILER=true CLEAN_BUILD=yes ./prepare_deps
