0140ae2d9ed140caaef9821e1f491c391d6e3d24
Tanzinul Islam <11808226+tanzislam@users.noreply.github.com>
2022-01-26 14:35:32 +0000

    Update prepare.sh

--- .trustinsoft/prepare.sh
+++ .trustinsoft/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y lsb-release libtool
+sudo apt-get update
+sudo apt-get install -y lsb-release libtool
 cd ..
-git fetch --prune --recurse-submodules=yes
-git submodule update --init --recursive
+git status
+git submodule status --recursive
 SKIP_COMPILER=true CLEAN_BUILD=yes ./prepare_deps
